75b31552f6dff7ee51924bd65919aa362f8d7976
lkm <1245602249@qq.com>
2024-07-13 18:17:12 +0800

    update

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -1,17 +1,17 @@
 <script setup lang='ts'>
 import { computed, ref } from 'vue'
-import { NAvatar, useModal, useMessage, NButton } from 'naive-ui'
-import { useUserStore } from '@/store'
+import { NAvatar } from 'naive-ui'
+import { useAuthStore, useUserStore } from '@/store'
 import defaultAvatar from '@/assets/avatar.jpg'
 import { isString } from '@/utils/is'
-import {Login} from '@/components/common'
-import { useAuthStore } from '@/store'
+import { Login } from '@/components/common'
 const userStore = useUserStore()
 const authStore = useAuthStore()
 const userInfo = computed(() => userStore.userInfo)
+var t = authStore.token == "" ||  authStore.token == undefined
 const show = ref(false)
-function showLoginPage(){
-  console.log("showpage")
+function showLoginPage() {
+  console.log('showpage')
 }
 </script>
 
@@ -19,21 +19,21 @@ function showLoginPage(){
   <div class="flex items-center overflow-hidden">
     <div class="w-10 h-10 overflow-hidden rounded-full shrink-0 login_btn" @click="show = true">
       <template v-if="isString(userInfo.avatar) && userInfo.avatar.length > 0">
-        <n-avatar
-        :style="{
-          color: 'white',
-          backgroundColor: 'black'
-        }"
-        size="large"
-      >
-    {{  authStore.token == "" ? '登录': 'AI'}} 
-  </n-avatar>
+        <NAvatar
+          :style="{
+            color: 'white',
+            backgroundColor: 'black',
+          }"
+          size="large"
+        >
+          {{ t? '登录' : 'AI' }}
+        </NAvatar>
       </template>
       <template v-else>
         <NAvatar size="large" round :src="defaultAvatar" />
       </template>
     </div>
-    <div class="flex-1 min-w-0 ml-2" v-if="false">
+    <div v-if="false" class="flex-1 min-w-0 ml-2">
       <h2 class="overflow-hidden font-bold text-md text-ellipsis whitespace-nowrap">
         <!-- {{ userInfo.name ?? 'LKM' }} -->
         LKM
@@ -42,6 +42,7 @@ function showLoginPage(){
   </div>
   <Login v-model:visible="show" />
 </template>
+
 <style>
   .login_btn {
     cursor: pointer;
